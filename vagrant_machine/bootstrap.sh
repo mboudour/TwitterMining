3800063b0a15d5748215845df856e9cc70ed17ac
Sergios Lenis <sergioslenis@gmail.com>
2016-06-12 00:37:52 +0300

    Added the vagrant config

--- vagrant_machine/bootstrap.sh
+++ vagrant_machine/bootstrap.sh
@@ -2,9 +2,14 @@
 
 apt-get update
 apt-get install -y python-pip python-dev build-essential 
+apt-get install libfreetype6-dev libpng-dev
 sudo -H pip install --upgrade pip
 sudo -H pip install jupyter
 sudo -H pip install pandas
+sudo apt-get install python-matplotlib
+sudo -H pip install --upgrade matplotlib
 pip install --user python-twitter
+pip install --user seaborn
+pip install --user lightning-python
 
 jupyter notebook --notebook-dir=/vagrant/notebook --no-browser --ip=0.0.0.0 &
